923d8d97649b7facee6bc2e45f262a97e6259ad4
AlexeyMelV <137533439+AlexeyMelV@users.noreply.github.com>
2023-09-30 22:12:14 +0400

    работоспособная версия Змейки

---
+++ SnakeGame/Point.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SnakeGame
+{
+  public readonly struct Point
+  {
+        public Point(int x, int y, ConsoleColor color)
+    {
+      X = x;
+      Y = y;
+      Color = color;
+    }
+
+    public int X { get; }
+    public int Y { get; }
+
+    public ConsoleColor Color { get; }
+
+    public void Print()
+    {
+      Console.SetCursorPosition(X, Y);
+      Console.Write('█');
+    }
+
+    public void Clear()
+    {
+      Console.SetCursorPosition(X, Y);
+      Console.Write(' ');
+    }
+  }
+}
